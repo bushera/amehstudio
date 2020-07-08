938059410dcc730ac19d3bf5bddcfe3a04729962
bushera <59298086+bushera@users.noreply.github.com>
2020-07-08 02:40:49 +0100

    upto

--- topbar.js
+++ topbar.js
@@ -1,100 +1,20 @@
-
-var preload-page = document.getElementById("preload-page");	
-
-window.addEventListener("load", function(){
-	preload-page.style.display = "none"; 
-});
- 
- /*function change(){
-  document.location.href = "http://amehegwuh.netlify.com"
-  }
-       
-
-   
-  $(function(){
-        $("#pichold").click(function(){
-               $("#buybox").slideToggle(500);
-               $("#buybox1").css("display", "none");
-               $("#buybox2").css("display", "none");
-               $("#buybox3").css("display", "none");
-        });
-        
-        $("#pichold1").click(function(){
-            $("#buybox1").slideToggle(500);
-            $("#buybox").css("display", "none");
-            $("#buybox2").css("display", "none");
-            $("#buybox3").css("display", "none");
-        });
-        
-        $("#pichold2").click(function(){
-            $("#buybox2").slideToggle(500);
-            $("#buybox").css("display", "none");
-            $("#buybox1").css("display", "none");
-            $("#buybox3").css("display", "none");
-        });
-        
-        $("#pichold3").click(function(){
-            $("#buybox3").slideToggle(500);
-            $("#buybox").css("display", "none");
-            $("#buybox1").css("display", "none");
-            $("#buybox2").css("display", "none");
-        });
-		
-		$("#topfloor").click(function(){
-			   $("#topbar").animate({left: "0%"}, 1000);
-			   $("#topfloor").css("display", "none");
-			   $("#topfloor3").css("display", "none");
-			   $("#topimg").css("width", "99%");
-			   $(".jumbotron").css("width", "95%");
-			   $("h1").animate({left: "208px"} ,1100);
-			   $("#topbar").css("box-shadow", "1px 1px 5px 1px rgba(0,0,0,.2)");
-			   $("#topfloor1").css("display", "block");
-			   $("#topfloor").off(click);
+ $(function(){
+		$("#offslide-btn").click(function(){
+			   $("#offslide").animate({left: "0%"}, 800);
+			   $("#offslide-btn").css("display", "none");
+			   $("#offslide-h1").animate({left: "15%"} ,900);
+			   $("#offslide-btn").off(click);
 			   
 		});
 		
 		
-		$("#topfloor1").click(function(){
-			   $("#topbar").animate({left: "-15%"}, 1000);
-			   $("#topfloor1").css("display", "none");
-			   $("#topimg").css("width", "90%");
-			   $(".jumbotron").css("width", "85%");
-			   $("h1").animate({left: "108px"}, 1100);
-			   $("#topbar").css("box-shadow", "1px 1px 5px 1px rgba(0,0,0,0)");
-			   $("#topfloor").slideToggle(1300);
-			   $("#topfloor").css("display", "block");
-			   $("#topfloor").off(click);
+		$("#offslide-close").click(function(){
+			   $("#offslide").animate({left: "-50%"}, 800);
+			   $("#offslide-btn").css("display", "block");
+			   $("#offslide-h1").animate({left: "-70%"}, 900);
+			   $("#offslide-close").off(click);
 		});	
 		
-		$("#topfloor2").click(function(){
-		$("#topbar").animate({left: "0%"}, 1000);
-		$("#topfloor2").css("display", "none");
-		$("#topfloor1").css("display", "none");
-		$("#topimg").css("width", "98%");
-		$(".jumbotron").css("width", "95%");
-		$("h1").animate({left: "208px"} ,1100);
-		$("#topbar").css("box-shadow", "1px 1px 5px 1px rgba(0,0,0,.2)");
-		$("#topfloor3").css("display", "block");
-		$("#topfloor2").off(click);
-		
-		});
-		
-		
-		$("#topfloor3").click(function(){
-		$("#topbar").animate({left: "-60%"}, 1000);
-		$("#topfloor3").css("display", "none");
-		$("#topimg").css("width", "85%");
-		$(".jumbotron").css("width", "85%");
-		$("h1").animate({left: "108px"}, 1100);
-		$("#topbar").css("box-shadow", "1px 1px 5px 1px rgba(0,0,0,0)");
-		$("#topfloor2").slideToggle(1300);
-		$("#topfloor2").css("display", "block");
-		$("#topfloor2").off(click);
-		});	
-		
-		$("#dropd").click(function(){
-		    $(".jumbotron").fadeToggle(1500);
-		});	
 			
-  })*/
-  
+  });
+
